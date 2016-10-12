61a73d4b36a357e6fa899c143035dc40eee8f76e
Alina Liburkina <alina.liburkina@students.fhnw.ch>
2016-10-12 10:26:53 +0200

    first methods

--- Mada/src/RSA_Main.java
+++ Mada/src/RSA_Main.java
@@ -1,8 +1,34 @@
+import java.math.BigInteger;
+import java.util.Random;
 
 public class RSA_Main {
 
+	private BigInteger generateProbablePrimeNumber() {
+		return BigInteger.probablePrime(1000, new Random());
+	}
+
+	private BigInteger primeNumber() {
+		BigInteger bigInt = generateProbablePrimeNumber();
+		if (bigInt.isProbablePrime(20))
+			return bigInt;
+		else {
+			return primeNumber();
+		}
+	}
+
+	private BigInteger generateN() {
+		BigInteger q = primeNumber();
+		BigInteger p = primeNumber();
+		if (q.equals(p))
+			generateN();
+		return p.multiply(q);
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		RSA_Main rsa = new RSA_Main();
+		BigInteger primeNumber;
+
+		System.out.println(rsa.primeNumber());
 
 	}
 
