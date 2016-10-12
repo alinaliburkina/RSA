72a711ae7565abdf25b6d8f48cd4d1e18286173a
Alina Liburkina <alina.liburkina@students.fhnw.ch>
2016-10-12 12:49:58 +0200

    euclid algorithm + generateD methods

--- Mada/src/RSA_Main.java
+++ Mada/src/RSA_Main.java
@@ -39,7 +39,53 @@ public class RSA_Main {
 
 	}
 
-	// Reads string to be decrypted from file 
+	private int generateE(BigInteger n) {
+		// TODO
+		return 0;
+	}
+
+	// Calculates d by using of euclid-algorithm
+	private int generateD(BigInteger n, int e) {
+	
+		return euclidAlgorithm(e, n)[2];
+	}
+
+	// Calculates by using of Euclid algorithm ggT(e,n) and
+	// Bezout-coefficient and returns them in the array. Position one is for
+	// ggT(e,n), position2 and position3 fpr coefficients.
+
+	private int[] euclidAlgorithm(int e, BigInteger n) {
+
+		// 1st Initialization
+		int a = e;
+		int b = n.intValue();
+		int x0 = 1;
+		int x1 = 0;
+		int y0 = 0;
+		int y1 = 1;
+		int q = a / b;
+		int r = a % b;
+
+		// 2nd Loop
+		while (b != 0) {
+			q = a / b;
+			r = a % b;
+			a = b;
+			b = r;
+			x0 = x1;
+			y0 = y1;
+			x1 = x0 - q * x1;
+			y1 = y0 - q * y1;
+		}
+
+		int[] array = new int[3];
+		array[0] = a;
+		array[1] = x0;
+		array[2] = x1;
+		return array;
+	}
+
+	// Reads string to be decrypted from file
 	public void readFile() {
 		try (BufferedReader br = new BufferedReader(new FileReader("text.txt"))) {
 			StringBuilder sb = new StringBuilder();
@@ -57,32 +103,32 @@ public class RSA_Main {
 			e.printStackTrace();
 		}
 	}
-	
-	//Writes public and private keys 
-	public void writeToFile(String fileName){
-		
-		if(fileName.equals("sk.txt") || fileName.equals("pk.txt")){
-		
-		 String text = "Hello world";
-	        BufferedWriter output = null;
-	        try {
-	            File file = new File(fileName);
-	            output = new BufferedWriter(new FileWriter(file));
-	            output.write(text);
-	        } catch ( IOException e ) {
-	            e.printStackTrace();
-	        } finally {
-	          if ( output != null ) {
-	            try {
-					output.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+
+	// Writes public and private keys
+	public void writeToFile(String fileName) {
+
+		if (fileName.equals("sk.txt") || fileName.equals("pk.txt")) {
+
+			String text = "Hello world";
+			BufferedWriter output = null;
+			try {
+				File file = new File(fileName);
+				output = new BufferedWriter(new FileWriter(file));
+				output.write(text);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				if (output != null) {
+					try {
+						output.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
-	          }
-	        }
-	    }
-	
+			}
+		}
+
 	}
 
 	public static void main(String[] args) {
