d2e9469f48f9352aa6c0a79a4bd8585fa5a34fcc
Alina Liburkina <alina.liburkina@students.fhnw.ch>
2016-10-12 10:38:49 +0200

    generateN method

--- Mada/src/RSA_Main.java
+++ Mada/src/RSA_Main.java
@@ -3,10 +3,14 @@ import java.util.Random;
 
 public class RSA_Main {
 
+	// Generates a prossible prime number
 	private BigInteger generateProbablePrimeNumber() {
 		return BigInteger.probablePrime(1000, new Random());
 	}
 
+	// Checks if a generated number is a prime number and returns a prime
+	// number.
+	// In the way of failure calls itself to make the next check
 	private BigInteger primeNumber() {
 		BigInteger bigInt = generateProbablePrimeNumber();
 		if (bigInt.isProbablePrime(20))
@@ -16,19 +20,21 @@ public class RSA_Main {
 		}
 	}
 
+	//Generates n
 	private BigInteger generateN() {
 		BigInteger q = primeNumber();
 		BigInteger p = primeNumber();
-		if (q.equals(p))
-			generateN();
-		return p.multiply(q);
+		if (!q.equals(p))
+			return p.multiply(q);
+		return null;
+
 	}
 
 	public static void main(String[] args) {
 		RSA_Main rsa = new RSA_Main();
 		BigInteger primeNumber;
 
-		System.out.println(rsa.primeNumber());
+		System.out.println(rsa.generateProbablePrimeNumber());
 
 	}
 
