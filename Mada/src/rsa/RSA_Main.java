fc7d79a66c821ed775816f5d35033e6b9c2b9452
Alina Liburkina <alina.liburkina@students.fhnw.ch>
2016-10-16 17:04:07 +0200

    some new algorithms

--- Mada/src/rsa/RSA_Main.java
+++ Mada/src/rsa/RSA_Main.java
@@ -1,3 +1,5 @@
+package rsa;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -39,26 +41,43 @@ public class RSA_Main {
 
 	}
 
+	public int fiOfN(BigInteger n) {
+		return n.intValueExact() - 1;
+	}
+
+	// Generates E which belongs to Z of Fi
 	private int generateE(BigInteger n) {
-		// TODO
-		return 0;
+		boolean flag = false;
+		int i = fiOfN(n);
+		int index = i - 1;
+		while (flag && i > 1) {
+			int[] array = euclidAlgorithm(index, n);
+			// if ggT(index,n)==1
+			if (array[0] == 1) {
+				flag = true;
+				return index;
+			}
+			i--;
+		}
+
+		return 1;
 	}
 
 	// Calculates d by using of euclid-algorithm
 	private int generateD(BigInteger n, int e) {
-	
+
 		return euclidAlgorithm(e, n)[2];
 	}
 
 	// Calculates by using of Euclid algorithm ggT(e,n) and
 	// Bezout-coefficient and returns them in the array. Position one is for
-	// ggT(e,n), position2 and position3 fpr coefficients.
+	// ggT(e,n), position2 and position3 for coefficients.
 
 	private int[] euclidAlgorithm(int e, BigInteger n) {
 
 		// 1st Initialization
 		int a = e;
-		int b = n.intValue();
+		int b = n.intValueExact();
 		int x0 = 1;
 		int x1 = 0;
 		int y0 = 0;
@@ -85,6 +104,38 @@ public class RSA_Main {
 		return array;
 	}
 
+	public int fastExponention(int x, BigInteger n, int e) {
+		String binaryE = Integer.toBinaryString(e);
+		int i = Integer.toBinaryString(e).length() - 1;
+		int[] binaryEArray = new int[i + 1];
+		for (int j = 0; j < binaryEArray.length; j++) {
+			binaryEArray[j] = Integer.valueOf(binaryE.substring(j, j + 1));
+		}
+		int h = 1;
+		int k = x;
+		int index = 0;
+		while (i >= 0) {
+			if (binaryEArray[index] == 1) {
+				h = h * k % n.intValueExact();
+				k = k * k % n.intValueExact();
+				i = i - 1;
+			}
+			index++;
+		}
+		return h;
+
+	}
+
+	// Writes to sk.txt file keypair (n,d)
+	public void saveSecretKey(String keyPair) {
+		writeToFile("sk.txt", keyPair);
+	}
+
+	// Writes to pk.txt file keypair (e,d)
+	public void savePublicKey(String keyPair) {
+		writeToFile("pk.txt", keyPair);
+	}
+
 	// Reads string to be decrypted from file
 	public void readFile() {
 		try (BufferedReader br = new BufferedReader(new FileReader("text.txt"))) {
@@ -105,16 +156,15 @@ public class RSA_Main {
 	}
 
 	// Writes public and private keys
-	public void writeToFile(String fileName) {
+	public void writeToFile(String fileName, String keyPair) {
 
 		if (fileName.equals("sk.txt") || fileName.equals("pk.txt")) {
 
-			String text = "Hello world";
 			BufferedWriter output = null;
 			try {
 				File file = new File(fileName);
 				output = new BufferedWriter(new FileWriter(file));
-				output.write(text);
+				output.write(keyPair);
 			} catch (IOException e) {
 				e.printStackTrace();
 			} finally {
@@ -134,8 +184,9 @@ public class RSA_Main {
 	public static void main(String[] args) {
 		RSA_Main rsa = new RSA_Main();
 		BigInteger primeNumber;
+		String binaryE = "01234";
 
-		System.out.println(rsa.generateProbablePrimeNumber());
+		System.out.println(Integer.valueOf(binaryE.substring(1, 2)));
 
 	}
 
